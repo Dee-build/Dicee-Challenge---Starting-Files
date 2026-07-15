2929c9622a29f85651f55976211cd0b3477fdcc0
adedayobabalola18-rgb <adedayobabalola18@gmail.com>
2026-07-15 11:53:01 +0100

    Add Dicee game files including HTML, CSS, JavaScript, and dice images
    - Created `dicee.html` for the main game interface with player sections and linked styles and scripts.
    - Added `styles.css` for styling the game layout, including container, text, and image styles.
    - Implemented `index.js` to handle random dice roll logic and update images based on the results.
    - Included six dice images (`dice1.png` to `dice6.png`) for visual representation of the dice rolls.

---
+++ index.js
@@ -0,0 +1,59 @@
+randomNumber = Math.random() * 6 + 1;
+randomNumber = Math.floor(randomNumber);
+
+if (randomNumber === 1) {
+    document.querySelector(".img1").setAttribute("src", "images/dice1.png")
+}
+
+else if (randomNumber === 2) {
+    document.querySelector(".img1").setAttribute("src", "images/dice2.png")
+}
+
+else if (randomNumber === 3) {
+    document.querySelector(".img1").setAttribute("src", "images/dice3.png")
+}
+
+else if (randomNumber === 4) {
+    document.querySelector(".img1").setAttribute("src", "images/dice4.png")
+}
+
+else if (randomNumber === 5) {
+    document.querySelector(".img1").setAttribute("src", "images/dice5.png")
+}
+else {
+    document.querySelector(".img1").setAttribute("src", "images/dice6.png")
+}
+
+
+
+
+randomNumber2 = Math.random() * 6 + 1;
+randomNumber2 = Math.floor(randomNumber2);
+
+if (randomNumber2 === 1) {
+    document.querySelector(".img2").setAttribute("src", "images/dice1.png")
+}
+
+else if (randomNumber2 === 2) {
+    document.querySelector(".img2").setAttribute("src", "images/dice2.png")
+}
+
+else if (randomNumber2 === 3) {
+    document.querySelector(".img2").setAttribute("src", "images/dice3.png")
+}
+
+else if (randomNumber2 === 4) {
+    document.querySelector(".img2").setAttribute("src", "images/dice4.png")
+}
+
+else if (randomNumber2 === 5) {
+    document.querySelector(".img2").setAttribute("src", "images/dice5.png")
+}
+else {
+    document.querySelector(".img2").setAttribute("src", "images/dice6.png")
+}
+
+
+
+
+
